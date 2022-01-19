e75622a5ffe114b4ddbe401bd39d982369ffcc5c
x1n32 <x1n32@students.keele.ac.uk>
2022-01-19 15:23:11 +0000

    very basic hash map exercise added

---
+++ src/main/java/github/x1n32/HashMapExercise.java
@@ -0,0 +1,17 @@
+package github.x1n32;
+import java.util.HashMap;
+import java.util.Iterator;
+
+
+public class HashMapExercise {
+    public static HashMap<Integer, String> testScores = new HashMap<>();
+
+
+
+    public static void main(String[] args){
+        testScores.put(1, "yes");
+
+
+    }
+
+}
