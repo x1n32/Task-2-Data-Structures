87631b0eac7982a9e4d9b77f6d44f684e9839698
x1n32 <x1n32@students.keele.ac.uk>
2022-01-24 14:01:41 +0000

    Copied tests but catch has exceptions. Text file deosn't update when pushing?

--- src/test/java/github/x1n32/HashMapExerciseTest.java
+++ src/test/java/github/x1n32/HashMapExerciseTest.java
@@ -92,6 +92,7 @@ public class HashMapExerciseTest {
 
 	@Nested
 	class HashMapTesting {
+
         @Test
 		@DisplayName("Create")
 		public void Create() {
@@ -221,14 +222,135 @@ public class HashMapExerciseTest {
 				System.out.println(fileReport + "\n");
 			}
 		  }
+    }
+
+
+	@Nested
+	class HashMapTestingWithReport {
+        @Test
+		@DisplayName("Create")
+		public void Create() {
+			System.out.println("Create list");
+			assertFalse(ItemDescriptions.isEmpty());
+ 		}
+
+		 @Test
+		 @DisplayName("checkValue")
+		 public void checkValue() {
+
+			try {
+				System.out.println("checking specific value in list");
+				String expected = "A map of the street...";
+				assertEquals(expected, ItemDescriptions.get("Map"), "This should match the same text as the value of map.");
+	
+			} catch (Exception e) {
+
+				System.out.println(e + "\n");
+			}
+
+		  }
+		  @Test
+		  @DisplayName("checkValue2")
+		  public void checkValue2() {
+			try {
+				
+				System.out.println("checking specific value in list 2");
+				String expected = "This new descpription";
+				ItemDescriptions.put("Item", "This new descpription");
+				assertEquals(expected, ItemDescriptions.get("Item"), "This should match the same text as the value of map.");
+	
+			} catch (Exception e) {
+				System.out.println(e + "\n");
+			}
+
+		   }
+
+		  @Test
+		  @DisplayName("testChangeinValue")
+		  public void testChangeinValue(){
+			try {
+				System.out.println("Testing new Description");
+				String newDescription = "Testing change in description";
+				ItemDescriptions.put("Wooden Shield", newDescription);
+	
+				assertEquals(newDescription, ItemDescriptions.get("Wooden Shield")
+				 );
+				 
+				
+			} catch (Exception e) {
+
+				System.out.println(e + "\n");
+			}
+			
+
+		  }
+
+		  @Test
+		  @DisplayName("testChangeinValue2")
+		  public void testChangeinValue2(){
+			  try {
+				System.out.println("Testing new Description 2");
+				ItemDescriptions.put("Item", "This descpription");
+				String newDescription = "Chango description";
+				ItemDescriptions.put("Item", newDescription);
+	
+				assertEquals(newDescription, ItemDescriptions.get("Item"));
+				 
+				  
+			  } catch (Exception e) {
+
+				System.out.println(e + "\n");
+			}
+
+		  }
+
+
+
+		  @Test
+		  @DisplayName("testFindEmptyValues")
+		  public void testFindEmptyValues(){
+			  try {
+				System.out.println("Testing find and replace empty values");
+				ItemDescriptions.put("empty", "");
+				HashMapExercise.findAndReplaceEmptyValue(ItemDescriptions);
+				assertEquals("Unknown Description", ItemDescriptions.get("empty"));
+			  } catch (Exception e) {
+
+				System.out.println(e + "\n");
+			}
+
+		  }
+
+		  @Test
+		  @DisplayName("testFindNullValues")
+		  public void testFindNullValues(){
+			try {
+				System.out.println("Testing find and replace null values");
+				ItemDescriptions.put("empty2", null);
+				HashMapExercise.findAndReplaceEmptyValue(ItemDescriptions);
+				assertEquals("Unknown Description", ItemDescriptions.get("empty2"));
+			} catch (Exception e) {
+
+				System.out.println(e + "\n");
+			}
+		  }
+
+		  @Test
+		  @DisplayName("testHashMapCreationUnderstanding")
+		  public void testHashMapCreationUnderstanding(){
+			try {
+				System.out.println("Testing if user can create a hashmap in the method");
+				System.out.println("testHashMapCreationUnderstanding");
+				assertEquals("Abby", HashMapExercise.createNewHashMap() );
+	
+			} catch (Exception e) {
+
+				System.out.println(e + "\n");
+			}
+		  }
 
 
 
     }
-
-
-
-
-
-
 }
+
